919daede449bca7273c78f1b700feffb2a6fe087
Hoshiyar Singh <hoshiyarhs9438625@gmail.com>
2020-08-21 20:45:29 +0530

    Add files via upload

---
+++ Default parameter & Keyword.py
@@ -0,0 +1,16 @@
+#Default parameters
+
+def say_hello(name= 'Darth Varder', emoji= ':)'):
+print(f'helloooo {name} {emoji}')
+
+#Positional arguments
+
+say_hello('Andrei', ':)')
+say_hello('Dan', ':)')
+say_hello('Emily', ':)')
+
+#Keyword arguments
+
+say_hello(name= 'Bibi', emoji= 'o')
+say_hello('Timmy')
+
